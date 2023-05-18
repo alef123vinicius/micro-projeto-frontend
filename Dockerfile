9c3b0f35b0304caf57cf78d34eefe52d87978491
alef123vinicius <alef123.vinicius@gmail.com>
2023-05-18 00:23:42 -0300

    correcao dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,9 @@
 FROM node:14.20.0 as node
 WORKDIR /app
 COPY . .
-RUN npm install @angular-devkit/schematics @angular-devkit/schematics-cli @angular-devkit/core @schematics/angular
-RUN npm install @angular/material
-RUN npm install --save @angular/material @angular/animations @angular/cdk
+#RUN npm install @angular-devkit/schematics @angular-devkit/schematics-cli @angular-devkit/core @schematics/angular
+#RUN npm install @angular/material
+#RUN npm install --save @angular/material @angular/animations @angular/cdk
 RUN npm install
 
 RUN npm run build:prod
--- src/app/servico/stados.service.ts
+++ src/app/servico/stados.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of, tap } from 'rxjs';
 import { Estados } from '../interfaces/states';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environments';
 
 @Injectable({
@@ -12,7 +12,12 @@ export class StadosService {
   constructor(private http: HttpClient) { }
 
   getServicoStados(): Observable<Estados[]> {
-    return this.http.get<Estados[]>(`${environment.REST_URL}/estados`)
+    const httpOptions = {
+      headers: new HttpHeaders({ 
+        'Access-Control-Allow-Origin':'*'
+      })
+    };
+    return this.http.get<Estados[]>(`${environment.REST_URL}/estados`, httpOptions)
       .pipe(
         tap({
           error: (error) => console.error(error)
